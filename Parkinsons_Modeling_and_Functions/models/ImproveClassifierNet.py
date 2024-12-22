b76ebdde60c182138357e807863803a0b3390008
Flavio Mangione <141431783+Flavio-Mangione@users.noreply.github.com>
2024-12-22 12:32:07 +0100

    Add files via upload

--- Parkinsons_Modeling_and_Functions/models/ImproveClassifierNet.py
+++ Parkinsons_Modeling_and_Functions/models/ImproveClassifierNet.py
@@ -20,7 +20,8 @@ class ImprovedDeepBinaryClassifierNet(nn.Module):
         self.layer3 = nn.Sequential(
             nn.Linear(128, 256),
             nn.BatchNorm1d(256),
-            nn.ReLU()
+            nn.ReLU(),
+            nn.Dropout(0.5)
         )
         
         self.layer4 = nn.Sequential(
@@ -30,10 +31,8 @@ class ImprovedDeepBinaryClassifierNet(nn.Module):
             nn.Dropout(0.5)
         )
 
-        self.fc1 = nn.Sequential(
-            nn.Dropout(0.5),
-            nn.Linear(512, 1)
-        )
+        self.fc1 = nn.Linear(512, 1)
+
         
         self.sigmoid = nn.Sigmoid()
         
